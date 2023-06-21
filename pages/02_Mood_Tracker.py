f4644570d0c8a5e29accdec7b89cd31577b4f7b3
harrychangjr <71932325+harrychangjr@users.noreply.github.com>
2023-06-21 17:17:19 +0800

    add nltk sentiment analysis to mood tracker with journal

--- pages/02_Mood_Tracker.py
+++ pages/02_Mood_Tracker.py
@@ -3,6 +3,8 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 import pandas as pd
 import matplotlib.pyplot as plt
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 
 
 legalease = Image.open("assets/LegalEase.jpg")
@@ -12,33 +14,41 @@ st.set_page_config(page_title="LegalEase - Mood Tracker", page_icon = "👨🏻�
 st.sidebar.image(legalease)
 
 st.header("LegalEase")
-st.subheader("Mood Tracker")
-st.write("*Track your daily mood and visualize your mood trends over time.*")
+st.subheader("Mood Tracker with Journal")
+st.write("*Track your daily mood, journal entries, and visualize mood trends over time.*")
 
-# Create a DataFrame to store mood data
-mood_data = pd.DataFrame(columns=["Date", "Mood"])
+# Create a DataFrame to store mood and journal data
+data = pd.DataFrame(columns=["Date", "Mood", "Journal"])
 
-# Input form to track mood
+# Input form to track mood and journal entry
 date = st.date_input("Date")
 mood = st.slider("Mood (1-10)", 1, 10)
+journal = st.text_area("Journal Entry")
 
-# Add mood data to DataFrame
-if st.button("Add Mood"):
-    mood_data = mood_data.append({"Date": date, "Mood": mood}, ignore_index=True)
-    st.success("Mood added successfully!")
+# Add mood and journal data to DataFrame
+if st.button("Add Entry"):
+    data = data.append({"Date": date, "Mood": mood, "Journal": journal}, ignore_index=True)
+    st.success("Entry added successfully!")
 
-# Display mood data table
-st.subheader("Mood Data")
-st.dataframe(mood_data)
+# Display mood and journal data table
+st.subheader("Mood and Journal Entries")
+st.dataframe(data)
+
+# Perform sentiment analysis on journal entries
+if not data.empty:
+    sid = SentimentIntensityAnalyzer()
+    data["Sentiment Score"] = data["Journal"].apply(lambda x: sid.polarity_scores(x)["compound"])
+    st.subheader("Sentiment Analysis of Journal Entries")
+    st.dataframe(data[["Date", "Journal", "Sentiment Score"]])
 
 # Display mood trends over time
 st.subheader("Mood Trends Over Time")
-if not mood_data.empty:
-    mood_data["Date"] = pd.to_datetime(mood_data["Date"])
-    mood_data.set_index("Date", inplace=True)
-    mood_data.sort_index(inplace=True)
+if not data.empty:
+    data["Date"] = pd.to_datetime(data["Date"])
+    data.set_index("Date", inplace=True)
+    data.sort_index(inplace=True)
     plt.figure(figsize=(10, 6))
-    plt.plot(mood_data.index, mood_data["Mood"])
+    plt.plot(data.index, data["Mood"])
     plt.xlabel("Date")
     plt.ylabel("Mood")
     plt.title("Mood Trends")
